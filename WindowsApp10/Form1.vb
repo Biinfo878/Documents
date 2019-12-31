9924be677661bda06469d48c3fd455e7d8562670
Biinfo878 <shsuccessbarname@gmail.com>
2019-12-31 19:56:24 +0330

    تست

--- WindowsApp10/Form1.vb
+++
@@ -1,9 +0,0 @@
-﻿Public Class Form1
-
-
-    Public Sub X()
-        Dim X As Int16 = TWSClassLibrary.AckSignalManagement.ACKSignal.AckError
-        Dim Z As Int16 = SMSCore.SmsSendReciveType.ForSend
-    End Sub
-
-End Class
